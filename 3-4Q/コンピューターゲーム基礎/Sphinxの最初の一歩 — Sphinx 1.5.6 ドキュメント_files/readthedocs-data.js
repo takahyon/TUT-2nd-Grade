8b055cbc6b3197b8c629402825b2a9f96c879ae0
Takamasa <takahyon@gmail.com>
2017-10-20 17:06:09 +0900

    [add] ADS講義資料追加

---
+++ 3-4Q/コンピューターゲーム基礎/Sphinxの最初の一歩 — Sphinx 1.5.6 ドキュメント_files/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "sphinx-ja",
+    version: "stable",
+    language: "ja",
+    subprojects: {},
+    canonical_url: "http://www.sphinx-doc.org/ja/stable/",
+    theme: "sphinx13",
+    builder: "sphinx",
+    docroot: "/languages/ja/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "14d88d75"
+  };
+  
